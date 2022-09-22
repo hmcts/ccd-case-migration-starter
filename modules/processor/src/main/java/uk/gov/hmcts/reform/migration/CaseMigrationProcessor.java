bd266efc487b366768c84fa06997d04ff506500c
Kiran Yenigala <kiran.yenigala@nihr.ac.uk>
2022-09-22 14:04:32 +0100

    CCD-3669: Logging added and improved migration behaviour

--- modules/processor/src/main/java/uk/gov/hmcts/reform/migration/CaseMigrationProcessor.java
+++ modules/processor/src/main/java/uk/gov/hmcts/reform/migration/CaseMigrationProcessor.java
@@ -3,14 +3,16 @@ package uk.gov.hmcts.reform.migration;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.migration.ccd.CoreCaseDataService;
+import uk.gov.hmcts.reform.migration.repository.ElasticSearchRepository;
+import uk.gov.hmcts.reform.migration.repository.IdamRepository;
 import uk.gov.hmcts.reform.migration.service.DataMigrationService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Component
@@ -25,11 +27,20 @@ public class CaseMigrationProcessor {
     @Autowired
     private DataMigrationService dataMigrationService;
 
+    @Autowired
+    private ElasticSearchRepository elasticSearchRepository;
+
+    @Autowired
+    private IdamRepository idamRepository;
+
     @Getter
     private List<Long> migratedCases = new ArrayList<>();
 
     @Getter
     private List<Long> failedCases = new ArrayList<>();
+
+    @Value("${migration.caseType}")
+    private String caseType;
 
     public void processSingleCase(String userToken, String caseId) {
         CaseDetails caseDetails;
@@ -39,32 +50,53 @@ public class CaseMigrationProcessor {
             log.error("Case {} not found due to: {}", caseId, ex.getMessage());
             return;
         }
-        if (dataMigrationService.accepts().test(caseDetails)) {
-            updateCase(userToken, caseDetails.getId(), caseDetails.getData());
-        } else {
-            log.info("Case {} already migrated", caseDetails.getId());
-        }
-    }
-
-    public void processCaseDetails(String userToken, CaseDetails caseDetails) {
-        updateCase(userToken, caseDetails.getId(), caseDetails.getData());
+        updateCase(userToken, caseDetails);
     }
 
     public void processAllCases(String userToken, String userId) {
         coreCaseDataService.fetchAll(userToken, userId).stream()
-            .forEach(caseDetails -> updateCase(userToken, caseDetails.getId(), caseDetails.getData()));
+            .forEach(caseDetails -> updateCase(userToken, caseDetails));
     }
 
-    private void updateCase(String authorisation, Long id, Map<String, Object> data) {
-        log.info("Updating case {}", id);
-        try {
-            log.debug("Case data: {}", data);
-            coreCaseDataService.update(authorisation, id.toString(), EVENT_ID, EVENT_SUMMARY, EVENT_DESCRIPTION, data);
-            log.info("Case {} successfully updated", id);
-            migratedCases.add(id);
-        } catch (Exception e) {
-            log.error("Case {} update failed due to: {}", id, e.getMessage());
-            failedCases.add(id);
+    public void migrateCases() {
+        log.info("Data migration of all cases started for case type: {}", caseType);
+        String userToken =  idamRepository.generateUserToken();
+        List<CaseDetails> listOfCaseDetails = elasticSearchRepository.findCasesWithOutHmctsSServiceId(userToken, caseType);
+        listOfCaseDetails.stream()
+            .forEach(caseDetails -> updateCase(userToken, caseDetails));
+        log.info("-----------------------------------------");
+        log.info("Data migration completed");
+        log.info("-----------------------------------------");
+        log.info("Total number of processed cases: {}", getMigratedCases().size() + getFailedCases().size());
+        log.info("Total number of migrations performed: {}", getMigratedCases().size());
+        log.info("-----------------------------------------");
+        log.info("Migrated cases: {} ", !getMigratedCases().isEmpty() ? getMigratedCases() : "NONE");
+        log.info("Failed cases: {}", !getFailedCases().isEmpty() ? getFailedCases() : "NONE");
+        log.info("Data migration of all cases completed");
+    }
+
+    private void updateCase(String authorisation, CaseDetails caseDetails) {
+        if (dataMigrationService.accepts().test(caseDetails)) {
+            Long id = caseDetails.getId();
+            log.info("Updating case {}", id);
+            try {
+                log.debug("Case data: {}", caseDetails.getData());
+                coreCaseDataService.update(
+                    authorisation,
+                    id.toString(),
+                    EVENT_ID,
+                    EVENT_SUMMARY,
+                    EVENT_DESCRIPTION,
+                    caseDetails.getData()
+                );
+                log.info("Case {} successfully updated", id);
+                migratedCases.add(id);
+            } catch (Exception e) {
+                log.error("Case {} update failed due to: {}", id, e.getMessage());
+                failedCases.add(id);
+            }
+        } else {
+            log.info("Case {} already migrated", caseDetails.getId());
         }
     }
 }
--- modules/processor/src/main/java/uk/gov/hmcts/reform/migration/CaseMigrationRunner.java
+++ modules/processor/src/main/java/uk/gov/hmcts/reform/migration/CaseMigrationRunner.java
@@ -2,16 +2,10 @@ package uk.gov.hmcts.reform.migration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.migration.repository.ElasticSearchRepository;
-import uk.gov.hmcts.reform.migration.repository.IdamRepository;
-
-import java.util.List;
 
 @Slf4j
 @SpringBootApplication
@@ -19,16 +13,7 @@ import java.util.List;
 public class CaseMigrationRunner implements CommandLineRunner {
 
     @Autowired
-    private ElasticSearchRepository elasticSearchRepository;
-
-    @Autowired
     private CaseMigrationProcessor caseMigrationProcessor;
-
-    @Autowired
-    private IdamRepository idamRepository;
-
-    @Value("${migration.caseType}")
-    private String caseType;
 
     public static void main(String[] args) {
         SpringApplication.run(CaseMigrationRunner.class, args);
@@ -37,12 +22,7 @@ public class CaseMigrationRunner implements CommandLineRunner {
     @Override
     public void run(String... args) {
         try {
-            log.info("Data migration of all cases started");
-            String userToken =  idamRepository.generateUserToken();
-            List<CaseDetails> listOfCaseDetails = elasticSearchRepository.findCasesWithOutHmctsSServiceId(userToken, caseType);
-            listOfCaseDetails.stream()
-                    .forEach(caseDetails -> caseMigrationProcessor.processCaseDetails(userToken, caseDetails));
-            log.info("Data migration of all cases completed");
+            caseMigrationProcessor.migrateCases();;
         } catch (Throwable e) {
             log.error("Migration failed with the following reason: {}", e.getMessage(), e);
         }
