8dbfb012069e0c5e13b3b66404264615f2c29d99
Kiran Yenigala <kiran.yenigala@nihr.ac.uk>
2022-09-22 23:17:45 +0100

    CCD-3669: Remove unused imports

--- modules/processor/src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
+++ modules/processor/src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.migration.ccd;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
