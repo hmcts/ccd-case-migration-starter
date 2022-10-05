01f1d0dfbd135d4e142e25167ec37cf6ba67061e
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2022-10-05 10:51:01 +0100

    Ccd 3665: Ensure that the code has a limit to the number of cases that can be processed per invocation (#30)
    * CCD-3665: Ensure that the code has a limit to the number of cases that can be processed per invocation
    
    * CCD-365: addressed code review comments
    
    Co-authored-by: Kiran Yenigala <kiran.yenigala@nihr.ac.uk>

--- src/test/java/uk/gov/hmcts/reform/migration/CaseMigrationProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/migration/CaseMigrationProcessorTest.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.migration;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.domain.exception.CaseMigrationException;
 import uk.gov.hmcts.reform.migration.ccd.CoreCaseDataService;
@@ -47,6 +49,11 @@ public class CaseMigrationProcessorTest {
     @Mock
     private IdamRepository idamRepository;
 
+    @Before
+    public void setUp() {
+        ReflectionTestUtils.setField(caseMigrationProcessor, "caseProcessLimit", 1);
+    }
+
     @Test
     public void shouldMigrateCasesOfACaseType() {
         when(dataMigrationService.accepts()).thenReturn(candidate -> true);
@@ -55,6 +62,32 @@ public class CaseMigrationProcessorTest {
         when(details.getId()).thenReturn(1677777777L);
         List<CaseDetails> caseDetails = new ArrayList<>();
         caseDetails.add(details);
+        when(elasticSearchRepository.findCaseByCaseType(USER_TOKEN, CASE_TYPE)).thenReturn(caseDetails);
+        List<CaseDetails> listOfCaseDetails = elasticSearchRepository.findCaseByCaseType(USER_TOKEN, CASE_TYPE);
+        assertNotNull(listOfCaseDetails);
+        when(coreCaseDataService.update(USER_TOKEN, EVENT_ID, EVENT_SUMMARY,
+                                        EVENT_DESCRIPTION, CASE_TYPE, details))
+            .thenReturn(details);
+        caseMigrationProcessor.migrateCases(CASE_TYPE);
+        verify(coreCaseDataService, times(1))
+            .update(USER_TOKEN,
+                    EVENT_ID,
+                    EVENT_SUMMARY,
+                    EVENT_DESCRIPTION,
+                    CASE_TYPE,
+                    details);
+    }
+
+    @Test
+    public void shouldMigrateOnlyLimitedNumberOfCases() {
+        when(dataMigrationService.accepts()).thenReturn(candidate -> true);
+        when(idamRepository.generateUserToken()).thenReturn(USER_TOKEN);
+        CaseDetails details = mock(CaseDetails.class);
+        when(details.getId()).thenReturn(1677777777L);
+        CaseDetails details1 = mock(CaseDetails.class);
+        List<CaseDetails> caseDetails = new ArrayList<>();
+        caseDetails.add(details);
+        caseDetails.add(details1);
         when(elasticSearchRepository.findCaseByCaseType(USER_TOKEN, CASE_TYPE)).thenReturn(caseDetails);
         List<CaseDetails> listOfCaseDetails = elasticSearchRepository.findCaseByCaseType(USER_TOKEN, CASE_TYPE);
         assertNotNull(listOfCaseDetails);
