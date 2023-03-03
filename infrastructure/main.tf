4a2bee2a489f72e2edc0a3e3eadcbd365df42ff7
JamesCollettCGI <james.collett@hmcts.net>
2023-03-03 21:17:49 +0000

    Add features {} to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,44 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  subscription_id            = local.cft_vnet[local.env].subscription
+  skip_provider_registration = "true"
+  features {}
+  alias = "cft_vnet"
+}
+
+locals {
+  s2s_url = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
+
+  env = var.env == "sandbox" ? "sbox" : var.env
+
+  cft_vnet = {
+    sbox = {
+      subscription = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+    }
+    perftest = {
+      subscription = "8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c"
+    }
+    aat = {
+      subscription = "96c274ce-846d-4e48-89a7-d528432298a7"
+    }
+    ithc = {
+      subscription = "62864d44-5da9-4ae9-89e7-0cf33942fa09"
+    }
+    preview = {
+      subscription = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
+    }
+    prod = {
+      subscription = "8cbc6f36-7c56-4963-9d36-739db5d00b27"
+    }
+    demo = {
+      subscription = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
